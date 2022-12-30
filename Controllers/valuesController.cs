edca54777c4ddb3d84442962bb1a92b865c1665e
Taqi Ahmed <taqiahmed339@gmail.com>
2022-12-30 22:18:11 +0500

    Add project files.

---
+++ Controllers/valuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace saafcity_fyp.Controllers
+{
+    public class valuesController : Controller
+    {
+        // GET: values
+        public string Index()
+        {
+            return "hello";
+        }
+        public string Name()
+        {
+            return "zainb";
+        }
+    }
+}
